265f947c70662dc4796773ca05b045b544bd6dbc
Sergey-Lyapin <sergeyl9pin@gmail.com>
2018-04-01 21:16:24 +0300

    Мелкие правки

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var hotels_button = document.querySelector(".main-hotels-button");
+var popup = document.querySelector(".modal-hotels");
+
+hotels_button.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.toggle("modal-close");
+  });
+
